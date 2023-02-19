81e97a4ef198326277486fd623e45239b54836eb
Thiago Corrêa <99252640+thiagocorreabotelho@users.noreply.github.com>
2023-02-19 14:36:49 -0300

    Subindo primeira versão com pasta e criação de projetos puris

---
+++ MeuPet/MeuPet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuPet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
